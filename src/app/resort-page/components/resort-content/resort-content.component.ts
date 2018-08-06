c6149d218b7ddb1df1505aaef4bd063126ee33fa
whc105 <whc309@nyu.edu>
2018-08-06 16:27:38 -0400

    Fixed a bug that causes google map to reload whenever a button is pressed

--- src/app/resort-page/components/resort-content/resort-content.component.ts
+++ src/app/resort-page/components/resort-content/resort-content.component.ts
@@ -13,9 +13,10 @@ import googleAPIKey from '../../../../resources/key';
 export class ResortContentComponent implements OnInit {
 
   @Input() resort: Resort;
+  private restaurantCollapse = false;
   private startingLocation: string = "New York City"
   private ticketPriceCostBasis;
-  private mapContent: string;
+  private mapContent: any;
   private distanceData: any;
   private nearbyRestaurants: any[] = [];
 
@@ -26,7 +27,8 @@ export class ResortContentComponent implements OnInit {
 
   ngOnInit() {
     this.ticketPriceCostBasis = this.resort.lift_tickets - 55;
-    this.mapContent = `https://www.google.com/maps/embed/v1/place?q=${this.resort.resort_name}&key=${googleAPIKey}`;
+    //Fix loop bug
+    this.mapContent = this.sanitizeURL(`https://www.google.com/maps/embed/v1/place?q=${this.resort.resort_name}&key=${googleAPIKey}`);
     this.fetchMapsDataService.getDistance(this.resort.resort_name).then((distance) => {
       this.distanceData = distance[0].elements[0];
     });
@@ -34,5 +36,9 @@ export class ResortContentComponent implements OnInit {
     this.fetchMapsDataService.getRating(this.resort.resort_name).then((nearbyRestaurants) => {
       this.nearbyRestaurants = nearbyRestaurants;
     })
+  }
+  
+  sanitizeURL(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
