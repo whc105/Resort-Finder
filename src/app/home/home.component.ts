1cf0ae9fc9bb6276f33616d8c8888f1fff20f83a
whc105 <whc309@nyu.edu>
2018-08-11 23:31:50 -0400

    Added a temporary landing page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public hoverFields: any = {
+    rockymtn: false,
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  flipOver(event, field) {
+    this.hoverFields[field] = false;
+  }
+
+  flipBack(event, field) {
+    this.hoverFields[field] = true;
   }
 
 }
