47b88960011fa12eae98a478e861131ac2e55bec
whc105 <whc309@nyu.edu>
2018-08-30 18:34:27 -0400

    Move

---
+++ devKeys.js
@@ -0,0 +1,7 @@
+const mongoUser = "hills-prod";
+const mongoPass = "8gdB93YwtZCLXgGt";
+
+module.exports = {
+    yelpAccessToken: "enlkgqwAREoM-cS6oj413-O68oDFDYvl4-zJTbSlbt8el4hKE2CrmFxJlQUEV5mLNvfvlxT3Nc_SU3yZGoWRkpJM1ZVGrkg93AemX1Vngd9d45e-CK6OeRmd35CBW3Yx",
+    mongoURI: `mongodb://${mongoUser}:${mongoPass}@ds018268.mlab.com:18268/hills-prod`
+}
