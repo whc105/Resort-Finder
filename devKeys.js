d9d66b77ae004df57920dc9bf330b75cec91c3a1
Wei Huan Chen <32426660+whc105@users.noreply.github.com>
2018-08-30 19:28:15 -0400

    Delete devKeys.js

--- devKeys.js
+++
@@ -1,7 +0,0 @@
-const mongoUser = "hills-prod";
-const mongoPass = "cc5YerUQ6K5nrvsp";
-
-module.exports = {
-    yelpAccessToken: "rfDX1Cn4pSLlnLJ8Lcc_4eMsJr7IZGOVNuBG2dclCWLY9v5DuLTMN6GOX7gUdF7kMqsV4x2OIfwzQC6VVBAUY_QVDkduYJsO663tLbO8VBvNdHxSA3G5HwsaKHyIW3Yx",
-    mongoURI: `mongodb://${mongoUser}:${mongoPass}@ds018268.mlab.com:18268/hills-prod`
-}
